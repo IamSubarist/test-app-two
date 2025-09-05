b15e2e1909924b1c2f158d7678be2bc13a0cc279
IamSubarist <optimistroma927@gmail.com>
2025-09-05 09:11:39 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -9,25 +9,6 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "google-fonts-cache",
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-              },
-              cacheKeyWillBeUsed: async ({ request }) => {
-                return `${request.url}?${Date.now()}`;
-              },
-            },
-          },
-        ],
-      },
       manifest: {
         name: "Test App Two",
         short_name: "TestApp",
